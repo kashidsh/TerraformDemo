dfec1a712b58eea36c701ae501bd8a0e60f57606
Shrikrishna Kashid <48318401+kashidsh@users.noreply.github.com>
2020-04-29 13:48:37 +0530

    Update main.tf

--- app-service-and-sql-database-var/main.tf
+++ app-service-and-sql-database-var/main.tf
@@ -2,20 +2,61 @@ provider "azurerm" {
   version = "=2.7.0"
   features {}
 }
-resource "azurerm_resource_group" "skexample" {
-  name     = "example-resources"
+
+resource "azurerm_resource_group" "LinktoIt-RG-Terraform" {
+  name     = "terraform-resource-group"
   location = "West Europe"
 }
 
-resource "azurerm_cognitive_account" "sk123example" {
-  name                = "example-account"
-  location            = azurerm_resource_group.skexample.location
-  resource_group_name = azurerm_resource_group.skexample.name
-  kind                = "Face"
+resource "azurerm_app_service_plan" "LinktoIt-ASP-TerraForm" {
+  name                = "terraform-appserviceplan"
+  location            = azurerm_resource_group.LinktoIt-RG-Terraform.location
+  resource_group_name = azurerm_resource_group.LinktoIt-RG-Terraform.name
 
-  sku_name = "S0"
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+}
+
+resource "azurerm_app_service" "LinktoIt-AS-Terraform" {
+  name                = "app-service-terraform"
+  location            = azurerm_resource_group.LinktoIt-RG-Terraform.location
+  resource_group_name = azurerm_resource_group.LinktoIt-RG-Terraform.name
+  app_service_plan_id = azurerm_app_service_plan.LinktoIt-ASP-TerraForm.id
+
+  site_config {
+    dotnet_framework_version = "v4.0"
+    scm_type                 = "LocalGit"
+  }
+
+  app_settings = {
+    "SOME_KEY" = "some-value"
+  }
+
+  connection_string {
+    name  = "Database"
+    type  = "SQLServer"
+    value = "Server=tcp:${azurerm_sql_server.test.fully_qualified_domain_name} Database=${azurerm_sql_database.test.name};User ID=${azurerm_sql_server.test.administrator_login};Password=${azurerm_sql_server.test.administrator_login_password};Trusted_Connection=False;Encrypt=True;"
+  }
+}
+
+resource "azurerm_sql_server" "LinktoIttest" {
+  name                         = "terraform-sqlserver"
+  resource_group_name          = azurerm_resource_group.LinktoIt-RG-Terraform.name
+  location                     = azurerm_resource_group.LinktoIt-RG-Terraform.location
+  version                      = "12.0"
+  administrator_login          = "houssem"
+  administrator_login_password = "4-v3ry-53cr37-p455w0rd"
+}
+
+resource "azurerm_sql_database" "LinktoIttest" {
+  name                = "terraform-sqldatabase"
+  resource_group_name = azurerm_resource_group.LinktoIt-RG-Terraform.name
+  location            = azurerm_resource_group.LinktoIt-RG-Terraform.location
+  server_name         = azurerm_sql_server.LinktoIttest.name
 
   tags = {
-    Acceptance = "Test"
+    environment = "production"
   }
 }
